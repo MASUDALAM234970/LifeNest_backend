5d57250d36d7e7096e1a4748c7485fc6fa7d0fc1
Masud Alam <mdmasudalam2349.com>
2026-07-27 23:54:42 +0600

    fix music playback error

--- config/settings.py
+++ config/settings.py
@@ -135,9 +135,15 @@ USE_I18N = True
 USE_TZ = True
 
 
-
-
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
+
 
 
 
